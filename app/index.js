18738978a5a2ec73475b8ed886052fdd97110911
Ivan Di Lelio <ivan.dilelio@redev.enterprise>
2024-10-12 17:55:03 +0200

    fix get AWS secret

--- app/index.js
+++ app/index.js
@@ -3,19 +3,36 @@ const env = process.env.ENV || 'dev';
 const { getSecret } = require('./config/index');
 const TelegramBot = require('node-telegram-bot-api');
 const reminders = require('./cron/reminders');
+const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
 
 (async () => {
     try {
+	// Crea un nuovo client SecretsManager
+	const client = new SecretsManagerClient({ region: "us-west-2" }); // Sostituisci con la tua regione AWS
+	// Funzione per recuperare il segreto
+	async function getSecret(secretName) {
+	  const command = new GetSecretValueCommand({ SecretId: secretName });
+	  try {
+	    const data = await client.send(command);
+	    if ("SecretString" in data) {
+	      return data.SecretString;
+	    } else {
+	      const buff = Buffer.from(data.SecretBinary, "base64");
+	      return buff.toString("ascii");
+	    }
+	  } catch (err) {
+	    console.error(err);
+	    throw err;
+	  }
+	}
         // Carica le configurazioni da AWS Secrets Manager
         const secrets = await getSecret('dev/telegram');
-
+	console.log("Il segreto è: ", secrets);
         // Configura il bot Telegram con il token
         const bot = new TelegramBot(secrets.TELEGRAM_KEY, { polling: true });
-
         // Setup del bot con listener per i messaggi
         bot.on('message', async (msg) => {
             const chatId = msg.chat.id;
-
             // Logica per la gestione dei messaggi o immagini caricate
             if (msg.photo) {
                 // Qui puoi richiamare la logica per il caricamento dell'immagine
@@ -24,12 +41,9 @@ const reminders = require('./cron/reminders');
                 bot.sendMessage(chatId, 'Benvenuto! Per favore, carica un\'immagine per partecipare.');
             }
         });
-
         console.log('DEDOBot avviato con successo!');
-
         // Avvia anche le routine cicliche per le notifiche
         reminders(bot, secrets);
-
     } catch (error) {
         console.error('Errore durante il caricamento delle configurazioni o l\'avvio del bot:', error);
     }
